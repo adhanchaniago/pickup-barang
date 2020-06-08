0988ce5fc7b5f29ed62bbdc7dc3de84eddaf9e25
hako-975 <andri.firman.saputra.56@gmail.com>
2020-06-09 03:01:47 +0700

    update database

--- pickup_barang.sql
+++ pickup_barang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 08 Jun 2020 pada 10.52
+-- Waktu pembuatan: 08 Jun 2020 pada 22.01
 -- Versi server: 10.4.11-MariaDB
 -- Versi PHP: 7.4.3
 
@@ -44,6 +44,28 @@ INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `layanan_paket`
+--
+
+CREATE TABLE `layanan_paket` (
+  `id_layanan_paket` int(11) NOT NULL,
+  `layanan_paket` varchar(100) NOT NULL,
+  `harga_layanan_paket` int(11) NOT NULL,
+  `durasi_pengiriman` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `layanan_paket`
+--
+
+INSERT INTO `layanan_paket` (`id_layanan_paket`, `layanan_paket`, `harga_layanan_paket`, `durasi_pengiriman`) VALUES
+(1, 'OKE (Ongkos Kirim Ekonomis)', 8000, 48),
+(2, 'YES (Yakin Esok Sampai)', 18000, 23),
+(3, 'REG (Regular)', 10000, 32);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `log`
 --
 
@@ -51,7 +73,7 @@ CREATE TABLE `log` (
   `id_log` int(11) NOT NULL,
   `isi_log` text NOT NULL,
   `tanggal_log` datetime NOT NULL,
-  `id_user` int(11) NOT NULL
+  `id_user` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -59,36 +81,42 @@ CREATE TABLE `log` (
 --
 
 INSERT INTO `log` (`id_log`, `isi_log`, `tanggal_log`, `id_user`) VALUES
-(1, 'Pengguna andri123 berhasil login', '2020-06-08 09:18:15', 1),
-(2, 'Pengguna  berhasil diubah', '2020-06-08 10:17:03', 1),
-(3, 'Pengguna  berhasil diubah', '2020-06-08 10:17:28', 1),
-(4, 'Pengguna  berhasil diubah', '2020-06-08 10:18:08', 1),
-(5, 'Pengguna  berhasil diubah', '2020-06-08 10:20:41', 1),
-(6, 'Pengguna andri123 berhasil logout', '2020-06-08 10:31:20', 1),
-(7, 'Pengguna andri123 berhasil login', '2020-06-08 10:31:25', 1),
-(8, 'Pengguna andri123 berhasil login', '2020-06-08 10:36:30', 1),
-(9, 'Pengguna andri123 berhasil login', '2020-06-08 10:39:59', 1),
-(10, 'Pengguna andri123 berhasil login', '2020-06-08 10:40:04', 1),
-(11, 'Pengguna andri123 berhasil login', '2020-06-08 10:40:38', 1),
-(12, 'Pengguna andri123 berhasil login', '2020-06-08 10:40:48', 1),
-(13, 'Pengguna andri123 berhasil logout', '2020-06-08 10:40:53', 1),
-(14, 'Pengguna andri123 berhasil login', '2020-06-08 10:40:57', 1),
-(15, 'Pengguna andri123 berhasil logout', '2020-06-08 10:41:00', 1),
-(16, 'Pengguna andri123 berhasil login', '2020-06-08 10:41:07', 1),
-(17, 'Pengguna andri123 berhasil logout', '2020-06-08 10:41:13', 1),
-(18, 'Pengguna andri123 berhasil login', '2020-06-08 10:41:17', 1),
-(19, 'Pengguna andri123 berhasil mengubah Password', '2020-06-08 10:44:13', 1),
-(20, 'Pengguna andri123 berhasil logout', '2020-06-08 10:44:25', 1),
-(21, 'Pengguna andri123 berhasil login', '2020-06-08 10:44:37', 1),
-(22, 'Pengguna andri123 berhasil mengubah Password', '2020-06-08 10:44:52', 1),
-(23, 'Pengguna andri123 berhasil login', '2020-06-08 10:45:21', 1),
-(24, 'Pengguna andri123 berhasil logout', '2020-06-08 10:45:24', 1),
-(25, 'Pengguna andri123 berhasil login', '2020-06-08 10:45:29', 1),
-(26, 'Pengguna andri123 berhasil login', '2020-06-08 10:45:46', 1),
-(27, 'Pengguna andri123 berhasil logout', '2020-06-08 10:47:31', 1),
-(28, 'Pengguna andri123 berhasil login', '2020-06-08 10:47:35', 1),
-(29, 'Pengguna  berhasil diubah', '2020-06-08 10:47:49', 1),
-(30, 'Pengguna  berhasil diubah', '2020-06-08 10:47:55', 1);
+(1, 'Pengguna andri123 berhasil logout', '2020-06-09 02:49:10', 1),
+(2, 'Pengguna andri123 berhasil login', '2020-06-09 02:49:16', 1),
+(3, 'Pengguna andri123 berhasil menambahkan Layanan Paket OKE (Ongkos Kirim Ekonomis)', '2020-06-09 02:52:24', 1),
+(4, 'Pengguna andri123 berhasil menambahkan Layanan Paket YES (Yakin Esok Sampai)', '2020-06-09 02:52:35', 1),
+(5, 'Pengguna andri123 berhasil menambahkan Layanan Paket REG (Regular)', '2020-06-09 02:52:45', 1),
+(9, 'Pelanggan Andri Firman Saputra berhasil menambahkan pesanan Buku Matematika SPM untuk SMK/MAK', '2020-06-09 02:54:20', NULL),
+(10, 'Pelanggan Andre berhasil menambahkan pesanan Gunting', '2020-06-09 03:00:36', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pickup_barang`
+--
+
+CREATE TABLE `pickup_barang` (
+  `id_pickup_barang` int(11) NOT NULL,
+  `nama_pengirim` varchar(100) NOT NULL,
+  `no_whatsapp_pengirim` varchar(25) NOT NULL,
+  `alamat_pengirim` text NOT NULL,
+  `nama_barang` text NOT NULL,
+  `berat_barang` float NOT NULL,
+  `jumlah_barang` int(4) NOT NULL,
+  `nama_penerima` varchar(100) NOT NULL,
+  `no_whatsapp_penerima` varchar(25) NOT NULL,
+  `alamat_penerima` text NOT NULL,
+  `tanggal_pemesanan` datetime NOT NULL,
+  `id_layanan_paket` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pickup_barang`
+--
+
+INSERT INTO `pickup_barang` (`id_pickup_barang`, `nama_pengirim`, `no_whatsapp_pengirim`, `alamat_pengirim`, `nama_barang`, `berat_barang`, `jumlah_barang`, `nama_penerima`, `no_whatsapp_penerima`, `alamat_penerima`, `tanggal_pemesanan`, `id_layanan_paket`) VALUES
+(5, 'Andri Firman Saputra', '087808675313', 'Jl. Amd Babakan Pocis No. 100 RT02/02', 'Buku Matematika SPM untuk SMK/MAK', 2, 1, 'Andre Farhan Saputra', '087878787878', 'Jl. Amd Babakan Pocis No. 69 RT02/02', '2020-06-09 02:54:20', 2),
+(6, 'Andre', '08780834222', 'Pocis no. 32', 'Gunting', 5, 10, 'Andri', '08787878787', 'Pocis No. 33', '2020-06-09 03:00:36', 2);
 
 -- --------------------------------------------------------
 
@@ -110,7 +138,8 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `nama_lengkap`, `img_profile`, `username`, `password`, `id_jabatan`) VALUES
-(1, 'Andri Firman Saputra', 'default2.png', 'andri123', '$2y$10$NYR5auwjiW4rpADsuDMukeO4JVTZ6rx0e7BNOorbFdYQyYB2J/ExS', 1);
+(1, 'Andri Firman Saputra', 'default2.png', 'andri123', '$2y$10$NYR5auwjiW4rpADsuDMukeO4JVTZ6rx0e7BNOorbFdYQyYB2J/ExS', 1),
+(3, 'Andre Farhan Saputra', 'avatar0411.png', 'andre123', '$2y$10$/mBJ.knd1vr4gKf/cbzk9Ofexvq4SD6SR.g4F5RfPCi5AXHSml2Lu', 2);
 
 --
 -- Indexes for dumped tables
@@ -123,11 +152,24 @@ ALTER TABLE `jabatan`
   ADD PRIMARY KEY (`id_jabatan`);
 
 --
+-- Indeks untuk tabel `layanan_paket`
+--
+ALTER TABLE `layanan_paket`
+  ADD PRIMARY KEY (`id_layanan_paket`);
+
+--
 -- Indeks untuk tabel `log`
 --
 ALTER TABLE `log`
   ADD PRIMARY KEY (`id_log`),
   ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `pickup_barang`
+--
+ALTER TABLE `pickup_barang`
+  ADD PRIMARY KEY (`id_pickup_barang`),
+  ADD KEY `id_layanan_paket` (`id_layanan_paket`);
 
 --
 -- Indeks untuk tabel `user`
@@ -144,23 +186,47 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `jabatan`
 --
 ALTER TABLE `jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `layanan_paket`
+--
+ALTER TABLE `layanan_paket`
+  MODIFY `id_layanan_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `log`
 --
 ALTER TABLE `log`
-  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id_log` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT untuk tabel `pickup_barang`
+--
+ALTER TABLE `pickup_barang`
+  MODIFY `id_pickup_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
+
+--
+-- Ketidakleluasaan untuk tabel `log`
+--
+ALTER TABLE `log`
+  ADD CONSTRAINT `log_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Ketidakleluasaan untuk tabel `pickup_barang`
+--
+ALTER TABLE `pickup_barang`
+  ADD CONSTRAINT `pickup_barang_ibfk_1` FOREIGN KEY (`id_layanan_paket`) REFERENCES `layanan_paket` (`id_layanan_paket`) ON DELETE NO ACTION ON UPDATE CASCADE;
 
 --
 -- Ketidakleluasaan untuk tabel `user`
