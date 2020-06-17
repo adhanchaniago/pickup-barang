0dc963dd007e25ea5796b12ec6fe8a01686156b3
hako-975 <andrifirmansaputra1@gmail.com>
2020-06-18 01:23:43 +0700

    little solved login problem

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -26,27 +26,27 @@
 		<div class="col-lg-5 mx-3 shadow rounded bg-primary text-white p-4">
 			<h2 class="heading_2 text-center">Pickup Barang</h2>
 			<h3 class="heading_3">Masuk</h3>
-			<form method="post">
-			  <div class="form-group">
-			    <label for="username"><i class="fas fa-fw fa-user"></i> Nama Pengguna</label>
-			    <input required type="text" autocomplete="off" id="username" class="form-control rounded-pill" name="username" value="<?= set_value('username'); ?>">
-			    <?= form_error('username', '<small class="form-text text-danger">', '</small>'); ?>
-			  </div>
-			  <div class="form-group">
-			    <label for="password"><i class="fas fa-fw fa-lock"></i> Kata Sandi</label>
-			    <input required type="password" id="password" class="form-control rounded-pill" name="password" value="<?= set_value('password'); ?>">
-			    <?= form_error('password', '<small class="form-text text-danger">', '</small>'); ?>
-			  </div>
-			  <div class="form-group">
-			  	<div class="row justify-content-center">
-			  		<!-- <div class="col text-left">
-			  			<a href="<?= base_url('assets/files/'); ?>" target="_blank" class="btn btn-danger rounded-pill"><i class="fas fa-fw fa-book"></i> Panduan</a>
-			  		</div> -->
-			  		<div class="col text-right">
-					    <button type="submit" name="login" class="btn btn-success rounded-pill"><i class="fas fa-fw fa-sign-in-alt"></i> Masuk</button>
-			  		</div>
+			<form method="post" action="<?= base_url('auth/login'); ?>">
+				<div class="form-group">
+				    <label for="username"><i class="fas fa-fw fa-user"></i> Nama Pengguna</label>
+				    <input required type="text" autocomplete="off" id="username" class="form-control rounded-pill" name="username" value="<?= set_value('username'); ?>">
+				    <?= form_error('username', '<small class="form-text text-danger">', '</small>'); ?>
 			  	</div>
-			  </div>
+			  	<div class="form-group">
+				    <label for="password"><i class="fas fa-fw fa-lock"></i> Kata Sandi</label>
+				    <input required type="password" id="password" class="form-control rounded-pill" name="password" value="<?= set_value('password'); ?>">
+				    <?= form_error('password', '<small class="form-text text-danger">', '</small>'); ?>
+			  	</div>
+			  	<div class="form-group">
+				  	<div class="row justify-content-center">
+				  		<div class="col text-left">
+				  			<a href="<?= base_url('/'); ?>" class="btn btn-info rounded-pill"><i class="fas fa-fw fa-arrow-left"></i> Kembali</a>
+				  		</div>
+				  		<div class="col text-right">
+						    <button type="submit" name="login" class="btn btn-success rounded-pill"><i class="fas fa-fw fa-sign-in-alt"></i> Masuk</button>
+				  		</div>
+				  	</div>
+			  	</div>
 			</form>
 		</div>
 	</div>
