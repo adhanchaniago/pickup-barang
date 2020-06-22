039c6c4e339efc73ad8d3bc96e616ada19d8a3eb
hako-975 <andri.firman.saputra.56@gmail.com>
2020-06-23 03:16:27 +0700

    pada halaman cek status pesanan, jika tombol status diklik akan

--- application/views/auth/cek_pesanan.php
+++ application/views/auth/cek_pesanan.php
@@ -83,30 +83,131 @@
     		</thead>
     		<tbody>
     			<?php $no=1;foreach ($pesanan as $key): ?>
-    			<tr>
-	    			<td><?= $no; ?></td>
-	    			<td><?= $key["no_resi"]; ?></td>
-	    			<td><?= $key["nama_penerima"]; ?></td>
-	    			<td><?= $key["no_wa_penerima"]; ?></td>
-	    			<td><?= $key["alamat_penerima"]; ?></td>
-	    			<td>
-	    				<strong>Nama Barang: </strong> <?= $key["nama_barang"]; ?>,  
-	    				<strong>Jumlah Barang: </strong> <?= number_format($key['jumlah_barang']); ?> Unit, 
-	    				<strong>Berat Barang: </strong> <?= number_format($key['berat_barang']); ?> Kg
-	    			</td>
-	    			<td>
-	    				<div class="text-center">
-	    				<a href="#" class="btn btn-sm <?= bg_status($key["id_status"]); ?> <?= text_status($key["id_status"]); ?>">
-	    					<i class="fas fa-fw <?= icon_status($key["id_status"]); ?>"></i>
-		    			</a>
-		    			</div>
-		    		</td>
-    			</tr>
+	    			<tr>
+		    			<td><?= $no; ?></td>
+		    			<td><?= $key["no_resi"]; ?></td>
+		    			<td><?= $key["nama_penerima"]; ?></td>
+		    			<td><?= $key["no_wa_penerima"]; ?></td>
+		    			<td><?= $key["alamat_penerima"]; ?></td>
+		    			<td>
+		    				<strong>Nama Barang: </strong> <?= $key["nama_barang"]; ?>,  
+		    				<strong>Jumlah Barang: </strong> <?= number_format($key['jumlah_barang']); ?> Unit, 
+		    				<strong>Berat Barang: </strong> <?= number_format($key['berat_barang']); ?> Kg
+		    			</td>
+		    			<td>
+		    				<div class="text-center">
+			    				<a href="#" data-target="#progressModal<?= $key['id_pickup_barang']; ?>" data-toggle="modal" class="btn btn-sm <?= bg_status($key["id_status"]); ?> <?= text_status($key["id_status"]); ?>">
+			    					<i class="fas fa-fw <?= icon_status($key["id_status"]); ?>"></i>
+				    			</a>
+			    			</div>
+			    		</td>
+	    			</tr>
     			<?php $no++;endforeach ?>
     		</tbody>
     	</table>
     </div>
 
+<?php foreach ($pesanan as $key): ?>
+<!-- Modal -->
+<div class="modal fade" id="progressModal<?= $key['id_pickup_barang']; ?>" tabindex="-1" role="dialog" aria-labelledby="progressModal<?= $key['id_pickup_barang']; ?>Label" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="progressModal<?= $key['id_pickup_barang']; ?>Label">Status Progress: <?= $key['nama_barang']; ?>, <?= $key['no_resi']; ?></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      	<div class="row my-2">
+			<div class="col">
+				<div class="text-white text-center rounded p-1 bg-danger"><i class="fas fa-2x fa-stopwatch"></i></div>
+			</div>
+			<div class="col">
+				<div class="text-white text-center rounded p-1 bg-warning"><i class="fas fa-2x fa-shipping-fast"></i></div>
+			</div>
+			<div class="col">
+				<div class="text-white text-center rounded p-1 bg-success"><i class="fas fa-2x fa-pallet"></i></div>
+			</div>
+			<div class="col">
+				<div class="text-white text-center rounded p-1 bg-primary"><i class="fas fa-2x fa-paper-plane"></i></div>
+			</div>
+		</div>
+        <div class="row text-center my-2">
+			<?php if ($key['id_status'] == '1'): ?>
+				<div class="col">
+					<div class="progress">
+	              	  <div class="progress-bar bg-danger" role="progressbar" style="width: 12%" aria-valuenow="12" aria-valuemin="0" aria-valuemax="100"></div>
+	                </div>
+				</div>
+			<?php elseif ($key['id_status'] == '2'): ?>
+				<div class="col">
+					<div class="progress">
+	              	  <div class="progress-bar bg-warning" role="progressbar" style="width: 37%" aria-valuenow="12" aria-valuemin="0" aria-valuemax="100"></div>
+	                </div>
+				</div>
+			<?php elseif ($key['id_status'] == '3'): ?>
+				<div class="col">
+					<div class="progress">
+	              	  <div class="progress-bar bg-success" role="progressbar" style="width: 63%" aria-valuenow="12" aria-valuemin="0" aria-valuemax="100"></div>
+	                </div>
+				</div>
+			<?php else: ?>
+				<div class="col">
+					<div class="progress">
+	              	  <div class="progress-bar bg-primary" role="progressbar" style="width: 100%" aria-valuenow="12" aria-valuemin="0" aria-valuemax="100"></div>
+	                </div>
+				</div>
+			<?php endif ?>
+		</div>
+		<div class="table-responsive my-2">
+			<table class="table table-bordered table-hover table-striped text-center">
+				<thead>
+					<tr>
+						<?php if ($key['id_status'] == '1'): ?>
+						<th>Pending</th>
+						<?php elseif ($key['id_status'] == '2'): ?>
+						<th>Pending</th>
+						<th>Kurir Menjemput</th>
+						<?php elseif ($key['id_status'] == '3'): ?>
+						<th>Pending</th>
+						<th>Kurir Menjemput</th>
+						<th>Barang Sampai Logistik</th>
+						<?php else: ?>
+						<th>Pending</th>
+						<th>Kurir Menjemput</th>
+						<th>Barang Sampai Logistik</th>
+						<th>No. Resi Terkirim</th>
+						<?php endif ?>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<?php if ($key['id_status'] == '1'): ?>
+							<td><?= $key['tanggal_pemesanan']; ?></td>
+						<?php elseif ($key['id_status'] == '2'): ?>
+							<td><?= $key['tanggal_pemesanan']; ?></td>
+							<td><?= $key['tanggal_penjemputan']; ?></td>
+						<?php elseif ($key['id_status'] == '3'): ?>
+							<td><?= $key['tanggal_pemesanan']; ?></td>
+							<td><?= $key['tanggal_penjemputan']; ?></td>
+							<td><?= $key['tanggal_masuk_logistik']; ?></td>
+						<?php else: ?>
+							<td><?= $key['tanggal_pemesanan']; ?></td>
+							<td><?= $key['tanggal_penjemputan']; ?></td>
+							<td><?= $key['tanggal_masuk_logistik']; ?></td>
+							<td><?= $key['tanggal_input_resi']; ?></td>
+						<?php endif ?>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+      </div>
+
+    </div>
+  </div>
+</div>
+<?php endforeach ?>
 
 <!-- 	<div class="row my-2">
 		<div class="col">
