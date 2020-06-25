41c78a81463ea8ed51a4b66d5842f8858213b892
hako-975 <andri.firman.saputra.56@gmail.com>
2020-06-25 21:25:11 +0700

    memperbaiki sedikit backup

--- application/controllers/BackupDB.php
+++ application/controllers/BackupDB.php
@@ -2,8 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class BackupDB extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Main_model', 'mm');
+	}
 	public function index()
 	{
+		$dataUser = $this->mm->getDataUser();
+		$this->mm->createLog('Pengguna ' . $dataUser['username'] . ' berhasil membuat Backup, untuk mengakses backup tambahkan /backups diakhir url, contoh https://pickupbarang.com/backups ', $dataUser['id_user']);
+
 		// Load the DB utility class
 		$this->load->dbutil();
 
@@ -13,10 +21,12 @@ class BackupDB extends CI_Controller {
 		// Load the file helper and write the file to your server
 		$this->load->helper('file');
 		$date = date('Y-m-d, H.i.s');
-		$tes = write_file('./backups/database-'.$date.'.zip', $backup);
+		write_file('./backups/database-'.$date.'.zip', $backup);
+		
 
 		// Load the download helper and send the file to your desktop
 		$this->load->helper('download');
 		force_download('database-'.$date.'.zip', $backup);
+
 	}
 }
